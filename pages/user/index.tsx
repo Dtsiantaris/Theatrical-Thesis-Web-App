5ff0bc5f4b02718e0ac12f6d068a9b2d8619c623
Dtsiantaris <dtsiantaris@gmail.com>
2023-10-29 21:22:25 +0200

    Add Card inside the user page.

--- pages/user/index.tsx
+++ pages/user/index.tsx
@@ -1,20 +1,102 @@
 // pages/user/profile.tsx
 import React from "react";
 import { useUserContext } from "../../src/contexts/UserContext";
-import { Typography } from "@material-ui/core";
+import {
+  Card,
+  CardContent,
+  Typography,
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  Chip,
+} from "@material-ui/core";
+import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
+import EnhancedEncryptionIcon from "@material-ui/icons/EnhancedEncryption";
+import AccountBalanceWalletIcon from "@material-ui/icons/AccountBalanceWallet";
+import MonetizationOnIcon from "@material-ui/icons/MonetizationOn";
+import EmailIcon from "@material-ui/icons/Email";
+import CheckCircleOutlineIcon from "@material-ui/icons/CheckCircleOutline";
+import HighlightOffIcon from "@material-ui/icons/HighlightOff";
 
 const UserProfile = () => {
   const { user } = useUserContext();
+
   if (!user) {
     return <p>Loading user data...</p>;
   }
 
+  const VerifiedChip: React.FC<{ isVerified: boolean }> = ({ isVerified }) => (
+    <Chip
+      icon={isVerified ? <CheckCircleOutlineIcon /> : <HighlightOffIcon />}
+      label={isVerified ? "Verified" : "Not Verified"}
+      color={isVerified ? "primary" : "secondary"}
+      variant="outlined"
+      size="small"
+    />
+  );
+
   return (
-    <div>
-      <Typography variant="h1" className="text-red-500">
-        {user.email}
-      </Typography>
-      {/* Render other user properties here */}
+    <div className="flex justify-center mt-10">
+      <Card className="w-96">
+        <CardContent>
+          <Typography variant="h5" gutterBottom>
+            User Profile
+          </Typography>
+          <List>
+            <ListItem>
+              <ListItemIcon>
+                <EmailIcon />
+              </ListItemIcon>
+              <ListItemText primary="Email" secondary={user.email} />
+              <VerifiedChip isVerified={user.emailVerified} />
+            </ListItem>
+            <ListItem>
+              <ListItemIcon>
+                <EnhancedEncryptionIcon />
+              </ListItemIcon>
+              <ListItemText primary="Two Factor" />
+              <VerifiedChip isVerified={user.twoFactorEnabled} />
+            </ListItem>
+            <ListItem>
+              <ListItemIcon>
+                <MonetizationOnIcon />
+              </ListItemIcon>
+              <ListItemText primary="Role" secondary={user.role} />
+            </ListItem>
+            <ListItem>
+              <ListItemIcon>
+                <AccountBalanceWalletIcon />
+              </ListItemIcon>
+              <ListItemText
+                primary="Balance"
+                secondary={`$${user.balance.toFixed(2)}`}
+              />
+            </ListItem>
+          </List>
+          <Divider className="my-2" />
+          <Typography variant="h6" gutterBottom>
+            Transactions
+          </Typography>
+          <List dense>
+            {user.transactions && user.transactions.length > 0 ? (
+              user.transactions.map((transaction, index) => (
+                <ListItem key={index}>
+                  <ListItemText
+                    primary={transaction.reason}
+                    secondary={`$${transaction.creditAmount.toFixed(2)}`}
+                  />
+                </ListItem>
+              ))
+            ) : (
+              <ListItem>
+                <ListItemText primary="No transactions available" />
+              </ListItem>
+            )}
+          </List>
+        </CardContent>
+      </Card>
     </div>
   );
 };
